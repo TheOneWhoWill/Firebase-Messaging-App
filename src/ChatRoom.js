0e4cdadf1caf4a23f9615c528baebf0d56becc82
SantiagoVira <8bitharrypotter@gmail.com>
2021-01-14 17:44:14 -0500

    added editing

--- src/ChatRoom.js
+++ src/ChatRoom.js
@@ -1,51 +1,75 @@
-import React, { useRef, useState } from 'react';
-import ChatMessage from './ChatMessage.js';
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import { useCollectionData } from 'react-firebase-hooks/firestore';
+import React, { useRef, useState } from "react";
+import ChatMessage from "./ChatMessage.js";
+import firebase from "firebase/app";
+import "firebase/firestore";
+import { useCollectionData } from "react-firebase-hooks/firestore";
 
 function ChatRoom(userInfo) {
-  const dummy = useRef();
-  const messagesRef = firebase.firestore().collection('messages');
-  const currentUser = userInfo.userInfo.currentUser;
-  const [formValue, setFormValue] = useState('');
-  const query = messagesRef.orderBy('createdAt', 'desc').limit(25);
-  const [messages] = useCollectionData(query, { idField: 'id' });
+    const dummy = useRef();
+    const messagesRef = firebase.firestore().collection("messages");
+    const currentUser = userInfo.userInfo.currentUser;
+    const [formValue, setFormValue] = useState("");
+    const query = messagesRef.orderBy("createdAt", "desc").limit(25);
+    const [messages] = useCollectionData(query, { idField: "id" });
 
-  const sendMessage = async (e) => {
-    console.log(currentUser)
-    e.preventDefault();
-    await messagesRef.add({
-      text: formValue,
-      user: currentUser.email,
-      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-      photoURL: currentUser.photoURL
-    })
-    .catch(function(error) {
-        console.error("Error adding document: ", error);
-    });
-    setFormValue('');
-    dummy.current.scrollIntoView({ behavior: 'smooth' });
-  }
+    const sendMessage = async (e) => {
+        console.log(currentUser);
+        e.preventDefault();
+        await messagesRef
+            .add({
+                text: formValue,
+                user: currentUser.email,
+                createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+                photoURL: currentUser.photoURL,
+            })
+            .catch(function (error) {
+                console.error("Error adding document: ", error);
+            });
+        setFormValue("");
+        dummy.current.scrollIntoView({ behavior: "smooth" });
+    };
 
-  function deleteMessage(msg) {
-    messagesRef.doc(msg.id).delete();
-  }
+    function deleteMessage(msg) {
+        messagesRef.doc(msg.id).delete();
+    }
 
-  return (
-    <div className="chatRoom">
-      <div className="messagesContainer">
-        {messages && messages.reverse().map(msg => <ChatMessage currentUser={currentUser} deleteMessage={() => deleteMessage(msg)} userName={msg.user} chatImg={msg.photoURL} messageText={msg.text} />)}
-        <span ref={dummy}></span>
-      </div>
-      <div className="formContainer">
-        <form>
-          <input className="inputBar" value={formValue} onChange={(e) => setFormValue(e.target.value)} type="text"/>
-          <button className="sendButton" disabled={!formValue} onClick={sendMessage}>Send</button>
-        </form>
-      </div>
-    </div>
-  );
+    return (
+        <div className="chatRoom">
+            <div className="messagesContainer">
+                {messages &&
+                    messages
+                        .reverse()
+                        .map((msg) => (
+                            <ChatMessage
+                                currentUser={currentUser}
+                                deleteMessage={() => deleteMessage(msg)}
+                                userName={msg.user}
+                                chatImg={msg.photoURL}
+                                messageText={msg.text}
+                                object={msg}
+                            />
+                        ))}
+                <span ref={dummy}></span>
+            </div>
+            <div className="formContainer">
+                <form>
+                    <input
+                        className="inputBar"
+                        value={formValue}
+                        onChange={(e) => setFormValue(e.target.value)}
+                        type="text"
+                    />
+                    <button
+                        className="sendButton"
+                        disabled={!formValue}
+                        onClick={sendMessage}
+                    >
+                        Send
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
 }
 
 export default ChatRoom;
