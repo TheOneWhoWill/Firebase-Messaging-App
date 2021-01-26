91d8a6580555cd9329ff02fa1e342313ba2c7a28
SantiagoVira <8bitharrypotter@gmail.com>
2021-01-26 13:40:01 -0500

    confirm delete

--- src/ChatRoom.js
+++ src/ChatRoom.js
@@ -3,65 +3,100 @@ import ChatMessage from "./ChatMessage.js";
 import firebase from "firebase/app";
 import "firebase/firestore";
 import { useCollectionData } from "react-firebase-hooks/firestore";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 
 function ChatRoom(userInfo) {
-  const dummy = useRef();
-  const messagesRef = firebase.firestore().collection("messages");
-  const currentUser = userInfo.userInfo.currentUser;
-  const [formValue, setFormValue] = useState("");
-  const query = messagesRef.orderBy("createdAt", "desc").limit(25);
-  const [messages] = useCollectionData(query, { idField: "id" });
+    const dummy = useRef();
+    const messagesRef = firebase.firestore().collection("messages");
+    const currentUser = userInfo.userInfo.currentUser;
+    const [formValue, setFormValue] = useState("");
+    const query = messagesRef.orderBy("createdAt", "desc").limit(25);
+    const [messages] = useCollectionData(query, { idField: "id" });
 
-  const sendMessage = async (e) => {
-    e.preventDefault();
-      await messagesRef.add({
-        text: formValue,
-        user: currentUser.email,
-        createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-        photoURL: currentUser.photoURL,
-      })
-      .catch(function (error) {
-        console.error("Error adding document: ", error);
-      });
-      setFormValue("");
-      dummy.current.scrollIntoView({ behavior: "smooth" });
-  };
-  
-  function edit(msg) {
-    const newVal = prompt("What would you like to change this message to?", msg.text);
-    const DocRef = messagesRef.doc(msg.id);
-    DocRef.get().then(function (doc) {if (doc.exists) {const saved = doc.data();if (newVal != null) {saved.text = newVal;}DocRef.set(saved);} else {console.log("No such document!");}}).catch(function (error) {console.log("Error getting document:", error);});
-  }
+    const sendMessage = async (e) => {
+        e.preventDefault();
+        await messagesRef
+            .add({
+                text: formValue,
+                user: currentUser.email,
+                createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+                photoURL: currentUser.photoURL,
+            })
+            .catch(function (error) {
+                console.error("Error adding document: ", error);
+            });
+        setFormValue("");
+        dummy.current.scrollIntoView({ behavior: "smooth" });
+    };
 
-  function deleteMessage(msg) {
-    messagesRef.doc(msg.id).delete();
-  }
+    function edit(msg) {
+        const newVal = prompt(
+            "What would you like to change this message to?",
+            msg.text
+        );
+        const DocRef = messagesRef.doc(msg.id);
+        DocRef.get()
+            .then(function (doc) {
+                if (doc.exists) {
+                    const saved = doc.data();
+                    if (newVal != null) {
+                        saved.text = newVal;
+                    }
+                    DocRef.set(saved);
+                } else {
+                    console.log("No such document!");
+                }
+            })
+            .catch(function (error) {
+                console.log("Error getting document:", error);
+            });
+    }
 
-  return (
-    <div className="chatRoom">
-      <div className="messagesContainer">
-        {messages && messages.reverse().map((msg) => (
-          <ChatMessage
-            currentUser={currentUser}
-            deleteMessage={() => deleteMessage(msg)}
-            editMessage={() => edit(msg)}
-            userName={msg.user}
-            chatImg={msg.photoURL}
-            messageText={msg.text}
-            object={msg}
-          />))}
-        <span ref={dummy}></span>
-      </div>
-      <div className="formContainer">
-        <form>
-          <input className="inputBar" value={formValue} onChange={(e) => setFormValue(e.target.value)} type="text"/>
-          <button className="sendButton" disabled={!formValue} onClick={sendMessage}><FontAwesomeIcon icon={faPaperPlane} /></button>
-        </form>
-      </div>
-    </div>
-  );
+    function deleteMessage(msg) {
+        if (window.confirm("Are you sure you want to delete this message?")) {
+            messagesRef.doc(msg.id).delete();
+        }
+    }
+
+    return (
+        <div className="chatRoom">
+            <div className="messagesContainer">
+                {messages &&
+                    messages
+                        .reverse()
+                        .map((msg) => (
+                            <ChatMessage
+                                currentUser={currentUser}
+                                deleteMessage={() => deleteMessage(msg)}
+                                editMessage={() => edit(msg)}
+                                userName={msg.user}
+                                chatImg={msg.photoURL}
+                                messageText={msg.text}
+                                object={msg}
+                            />
+                        ))}
+                <span ref={dummy}></span>
+            </div>
+            <div className="formContainer">
+                <form>
+                    <input
+                        className="inputBar"
+                        value={formValue}
+                        onChange={(e) => setFormValue(e.target.value)}
+                        type="text"
+                    />
+                    <button
+                        className="sendButton"
+                        disabled={!formValue}
+                        onClick={sendMessage}
+                    >
+                        <FontAwesomeIcon icon={faPaperPlane} />
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
 }
 
 export default ChatRoom;
